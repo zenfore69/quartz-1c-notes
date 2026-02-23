75b3360b71d91c6cd7577847050619251e7f563a
zenfore1 <naxim2006@gmail.com>
2026-02-23 13:44:09 +0300

    Quartz sync: Feb 23, 2026, 1:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "/quartz-1c-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
