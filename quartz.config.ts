db6901e6133144165479edfc604dde544ad6cd53
zenfore1 <naxim2006@gmail.com>
2026-02-23 13:23:08 +0300

    first

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Конспекты по 1С",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
